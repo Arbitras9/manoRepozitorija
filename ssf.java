8a6eec06dbced52e7c19286a58e338b44487ceb7
Arbitras9 <44171413+Arbitras9@users.noreply.github.com>
2018-10-16 17:19:48 +0300

    rrrr

--- ssf.java
+++ ssf.java
@@ -1,10 +1,9 @@
-package helloWorld;
 
-public class Antras {
+public class ssf {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println('ab');
 	}
 
 }
